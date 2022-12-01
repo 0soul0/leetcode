fb3bc5b9e15ab90a9fef78f80dc8a296680a4496
Shu ya <sevideo6666@gmail.com>
2022-12-01 11:50:03 +0800

    first commit

---
+++ dp/VowelCheck.java
@@ -0,0 +1,27 @@
+package dp;
+
+//1704. Determine if String Halves Are Alike
+public class VowelCheck {
+
+    public static void main(String[] args) {
+        System.out.println(halvesAreAlike("book")); // true
+        System.out.println(halvesAreAlike("textbook")); // false
+        System.out.println(halvesAreAlike("Uo")); // true
+    }
+
+    public static boolean halvesAreAlike(String s) {
+        return vowelCount(0, s.length() / 2, s) == vowelCount(s.length() / 2, s.length(), s);
+    }
+
+    public static int vowelCount(int start, int end, String s) {
+        String vowel = "aeiouAEIOU";
+        int count = 0;
+        for (int i = start; i < end; i++) {
+            if (vowel.indexOf(s.charAt(i)) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}

839a5f128118694c53596f41d0e0adb377cfda57
Shu ya <sevideo6666@gmail.com>
2023-01-07 12:05:53 +0800

    1/7

---
+++ dp/gasStation134.java
@@ -0,0 +1,19 @@
+package dp;
+
+public class gasStation134 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int ans = 0;
+        int curr = 0;
+        int total = 0;
+        for (int i = 0; i < gas.length; i++) {
+            curr += (gas[i] - cost[i]);
+            if (curr < 0) {
+                curr = 0;
+                ans = i + 1;
+            }
+            total += (gas[i] - cost[i]);
+        }
+
+        return total < 0 ? -1 : ans;
+    }
+}

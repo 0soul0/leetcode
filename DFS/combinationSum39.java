b1417bdac613075b69e2d0ff5022ce7d6bc29168
Shu ya <sevideo6666@gmail.com>
2023-01-16 11:27:47 +0800

    0116

---
+++ DFS/combinationSum39.java
@@ -0,0 +1,33 @@
+package DFS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//https://leetcode.com/problems/combination-sum/description/
+//39. Combination Sum
+public class combinationSum39 {
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        helper(0, candidates, target, new ArrayList<Integer>());
+        return res;
+    }
+
+    private void helper(int index, int[] candidates, int target, List<Integer> list) {
+        if (target == 0) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+            if (target - candidates[i] < 0)
+                break;
+            list.add(candidates[i]);
+            helper(i, candidates, target - candidates[i], list);
+            list.remove(list.size() - 1);
+        }
+
+    }
+}
---
+++ dp/longestMountainInArray845.java
@@ -0,0 +1,40 @@
+package dp;
+
+//https://leetcode.com/problems/longest-mountain-in-array/description/
+//845. Longest Mountain in Array
+public class longestMountainInArray845 {
+    public int longestMountain(int[] arr) {
+        int res = 0;
+        int path = 1;
+        Boolean up = true;
+        for (int i = 1; i < arr.length; i++) {
+
+            if (up && arr[i - 1] < arr[i]) {
+                path++;
+                continue;
+            }
+
+            if (arr[i - 1] > arr[i]) {
+                if (path == 1)
+                    continue;
+                up = false;
+                path++;
+                continue;
+            }
+
+            if (!up)
+                res = Math.max(res, path);
+
+            path = 2;
+            if (arr[i - 1] == arr[i])
+                path = 1;
+            up = true;
+
+        }
+
+        if (!up)
+            res = Math.max(res, path);
+        return res > 1 ? res : 0;
+
+    }
+}

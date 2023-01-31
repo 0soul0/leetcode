d84e43f6a3de79bc27a7a121851b51c27c857e11
Shu ya <sevideo6666@gmail.com>
2023-01-31 12:07:22 +0800

    0131

---
+++ dp/FlipStringToMonotoneIncreasing926.java
@@ -0,0 +1,28 @@
+package dp;
+
+//https://leetcode.com/problems/flip-string-to-monotone-increasing/description/
+//926. Flip String to Monotone Increasing
+public class FlipStringToMonotoneIncreasing926 {
+    public int minFlipsMonoIncr(String s) {
+        int zeroCount = 0;
+        int oneCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                zeroCount++;
+            }
+        }
+        int ans = s.length() - zeroCount;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '0') {
+                zeroCount--;
+            } else if (c == '1') {
+                ans = Math.min(oneCount + zeroCount, ans);
+                oneCount++;
+            }
+
+        }
+        return ans;
+    }
+}
---
+++ dp/bestTeamWithNoConflicts1626.java
@@ -0,0 +1,36 @@
+package dp;
+
+import java.util.Arrays;
+
+// 1626. Best Team With No Conflicts
+// https://leetcode.com/problems/best-team-with-no-conflicts/description/
+// LIS
+public class bestTeamWithNoConflicts1626 {
+    public int bestTeamScore(int[] scores, int[] ages) {
+        final int n = ages.length;
+        int[][] ageScorePair = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            ageScorePair[i][0] = ages[i];
+            ageScorePair[i][1] = scores[i];
+        }
+
+        Arrays.sort(ageScorePair, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+
+        int answer = 0;
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = ageScorePair[i][1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (ageScorePair[i][1] >= ageScorePair[j][1]) {
+                    dp[i] = Math.max(dp[i], ageScorePair[i][1] + dp[j]);
+                }
+            }
+            answer = Math.max(answer, dp[i]);
+        }
+        return answer;
+    }
+}
